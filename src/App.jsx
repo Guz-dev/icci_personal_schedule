4bd0a38e0f5cf44ff6fdcba59b782bc5265c34a9
Gustavo <tavo.3900@gmail.com>
2023-05-29 22:12:10 -0400

    Fetching de datos con el vps

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+import { getData } from './services/getData'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+
+  //CODIGO PARA LOS DATOS DE LA API
+  useEffect(() => {
+    getData().then(data => setData(data));
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data.map((item,index) => (
+        <div key={index}>
+          <h1>{item.semestre}</h1>
+          <p>{item.bloques_horario.profesor}</p>
+        </div>
+        
+        ))}
     </>
   )
 }
