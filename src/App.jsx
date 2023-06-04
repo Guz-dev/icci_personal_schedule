a7469f7415b417f13c996ea4da344edd3340d834
Gustavo <tavo.3900@gmail.com>
2023-06-03 23:59:12 -0400

    Revert

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
+<<<<<<< HEAD
 import Bloque_horario from "./components/bloque_horario"
 import MissingDataAlert from "./components/MissingDataAlert"
 import { getData } from "./services/middleware_db"
@@ -23,10 +26,16 @@ function App() {
   useEffect(() => {
     getData().then(data => setData(data));
   },[])
+=======
+
+function App() {
+  const [count, setCount] = useState(0)
+>>>>>>> parent of 4bd0a38 (Fetching de datos con el vps)
 
 
   
   return (
+<<<<<<< HEAD
 <>
       {bloquesState ? <>
         <div >
@@ -95,6 +104,29 @@ function App() {
         
         : <MissingDataAlert />
       }
+=======
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+>>>>>>> parent of 4bd0a38 (Fetching de datos con el vps)
     </>
   )
 }
